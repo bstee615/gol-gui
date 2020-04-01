7904917ba5f81e648f65a6491ddf817aeabbc0b1
Ben Steenhoek <benjis@iastate.edu>
2020-04-01 15:47:35 -0500

    Add docs

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gol-gui
-A GUI for Conway's Game of  Life that I wrote as a TA, used as an exercise in COM S 227.
+A Java Swing GUI for Conway's Game of  Life that I wrote as a TA, used as an exercise in COM S 227.
+Written in Java 8 to conform with the class's policy.
+
+## Running:
+
+Use Eclipse 2019-12 to run the program.
+Run `Program.java` to see a basic example.
--- src/hw3/display/LifeGui.java
+++ src/hw3/display/LifeGui.java
@@ -12,6 +12,7 @@ import hw3.Life;
  */
 public class LifeGui {
 	/**
+	 * Launch with specified rules.
 	 * 
 	 * @param grid    A grid defining the initial state of the simulation.
 	 * @param born    Rules defining when cells should be born.
@@ -19,6 +20,23 @@ public class LifeGui {
 	 */
 	public static void launchGui(String[] grid, int[] born, int[] survive) {
 		Life simulation = new Life(grid, born, survive);
+		LifePanel lifePanel = new LifePanel(simulation);
+		ControlPanel controlPanel = new ControlPanel(lifePanel);
+
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new MainFrame(lifePanel, controlPanel);
+			}
+		});
+	}
+
+	/**
+	 * Launch with specified Life instance.
+	 * 
+	 * @param simulation An instance of Life.
+	 */
+	public static void launchGui(Life simulation) {
 		LifePanel lifePanel = new LifePanel(simulation);
 		ControlPanel controlPanel = new ControlPanel(lifePanel);
 
