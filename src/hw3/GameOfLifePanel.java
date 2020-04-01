39e37c6ea6eba132981f8823d0bd7983e871ae5d
Ben Steenhoek <benjis@iastate.edu>
2020-04-01 09:25:23 -0500

    Remove spinner and make grid prettier

--- src/hw3/GameOfLifePanel.java
+++ src/hw3/GameOfLifePanel.java
@@ -1,57 +1,83 @@
 package hw3;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 
 import javax.swing.JPanel;
 
 public class GameOfLifePanel extends JPanel {
 
 	/**
-	 * 
+	 * Unique Serialization ID
 	 */
 	private static final long serialVersionUID = 1L;
-	private int gridSize;
-	
+	public static final int gridSize = 10;
+	public static final int minimumWidth = 50 * gridSize;
+
+	private static final Color inactive = Color.lightGray;
+	private static final Color active = Color.BLACK;
+
 	private Life simulation;
 
 	public GameOfLifePanel(Life simulation) {
-        this.simulation = simulation;
-        setVisible(true);
+		this.simulation = simulation;
+		setSize(getPreferredSize());
+		setVisible(true);
 	}
-	
-	public void setGridSize(int newGridSize) {
-		gridSize = newGridSize;
-	}
-	
+
 	public void nextGeneration() {
 		simulation.nextGeneration();
 		repaint();
 	}
-	
+
 	@Override
 	public Dimension getPreferredSize() {
-		return new Dimension(
-				gridSize * simulation.getColumns() + 1,
-				gridSize * simulation.getRows() + 1);
+		int squareWidth = Math.max(simulation.getRows(), simulation.getColumns());
+		Dimension fromContent = new Dimension(gridSize * squareWidth + 1, gridSize * squareWidth + 1);
+		Dimension minimum = new Dimension(minimumWidth, minimumWidth);
+
+		if (squareWidth < 600) {
+			return minimum;
+		} else {
+			return fromContent;
+		}
 	}
-	
+
+	public int getGridWidth() {
+		return getPreferredSize().width / gridSize;
+	}
+
+	public int getGridHeight() {
+		return getPreferredSize().height / gridSize;
+	}
+
 	private void drawGrid(Graphics g) {
-        for (int x = 0; x < simulation.getColumns(); x += 1)
-            for (int y = 0; y < simulation.getRows(); y += 1)
-            	if (simulation.getCell(x, y).isAlive()) {
-                    g.fillRect(gridSize * x, gridSize * y, gridSize, gridSize);	
-            	}
-            	else {
-                    g.drawRect(gridSize * x, gridSize * y, gridSize, gridSize);
-            	}
+		for (int row = getGridWidth() - 1; row >= 0; row -= 1) {
+			for (int col = getGridHeight() - 1; col >= 0; col -= 1) {
+				Rectangle gridSquare = new Rectangle(gridSize * col, gridSize * row, gridSize, gridSize);
+
+				if (row < simulation.getRows() && col < simulation.getColumns()) {
+					g.setColor(active);
+
+					if (simulation.getCell(row, col).isAlive()) {
+						g.fillRect(gridSquare.x, gridSquare.y, gridSquare.width, gridSquare.height);
+					} else {
+						g.drawRect(gridSquare.x, gridSquare.y, gridSquare.width, gridSquare.height);
+					}
+				} else {
+					g.setColor(inactive);
+					g.drawRect(gridSquare.x, gridSquare.y, gridSquare.width, gridSquare.height);
+				}
+			}
+		}
 	}
 
 	@Override
-    public void paint(Graphics g) {
-    	g.clearRect(0,  0,  getWidth(), getHeight());
-    	drawGrid(g);
-    }
-
+	public void paint(Graphics g) {
+		g.clearRect(0, 0, getWidth(), getHeight());
+		drawGrid(g);
+	}
 
 }
--- src/hw3/Program.java
+++ src/hw3/Program.java
@@ -4,26 +4,30 @@ import javax.swing.SwingUtilities;
 
 public class Program {
 
+	// @formatter:off
 	public static final String[] grid = {
-			"0 0 0 0 0 0", 
-			  "0 1 1 0 0 0", 
-			  "0 1 1 0 0 0", 
-			  "0 0 0 1 1 0",
-			  "0 0 0 1 1 0",
-			  "0 0 0 0 0 0"};
-	public static final int[] born = {3};
-	public static final int[] survive = {2,3};
-	
-    public static void main(String args[]) {
-    	Life simulation = new Life(grid, born, survive);
-    	GameOfLifePanel golPanel = new GameOfLifePanel(simulation);
-    	
+			"0 0 0 0 0 0",
+			"0 1 1 0 0 0",
+			"0 1 1 0 0 0",
+			"0 0 0 1 1 0",
+			"0 0 0 1 1 0",
+			"0 0 0 0 0 0" };
+	// @formatter:on
+
+	public static final int[] born = { 3 };
+	public static final int[] survive = { 2, 3 };
+
+	public static void main(String args[]) {
+		Life simulation = new Life(grid, born, survive);
+		GameOfLifePanel golPanel = new GameOfLifePanel(simulation);
+		ControlPanel controlPanel = new ControlPanel(golPanel);
+
 		SwingUtilities.invokeLater(new Runnable() {
-		    public void run()
-		    {
-		    	new MainFrame(golPanel);
-		    }
-	    });
-    }
+			@Override
+			public void run() {
+				new MainFrame(golPanel, controlPanel);
+			}
+		});
+	}
 
 }
